4cb058349f46cbf0f0186e1036769dac83c63e91
David Alves Dutra <dadutra@hotmail.com>
2025-02-13 15:16:54 -0300

    Adicionadas classes-base e metodos em Principal.

---
+++ src/main/java/org/example/classes/Principal.java
@@ -0,0 +1,26 @@
+package org.example.classes;
+
+public class Principal {
+
+    public static void inserirFuncionarios() {}
+
+    public static void removerJoao() {}
+
+    public static void imprimirTodos() {}
+
+    public static void concederAumento() {}
+
+    public static void agruparMap() {}
+
+    public static void imprimirPorFuncao() {}
+
+    public static void imprimirAniversariantes() {}
+
+    public static void imprimirMaisVelho() {}
+
+    public static void imprimirListaOrdenada() {}
+
+    public static void imprimirTotalSalarios() {}
+
+    public static void imprimirPercentualPorSalarioMinimo() {}
+}
